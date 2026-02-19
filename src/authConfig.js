4b30ff2bfd77258af5a412f779ced4c6d4aaf34d
Elimurray <eli.murr04@gmail.com>
2026-02-19 13:54:00 +1300

    uri

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "2938ea9f-9799-425b-ae4a-f8e5aef91c7f",
     authority: "https://login.microsoftonline.com/common",
-    redirectUri: "hiab.azzudo.com",
+    redirectUri: "https://hiab.azzudo.com",
     navigateToLoginRequestUrl: false,
   },
   cache: {
